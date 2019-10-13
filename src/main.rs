532ba022ac01d0c5a388607ab3e781526b46744a
Ubuntu <ubuntu@ec2-35-173-185-145.compute-1.amazonaws.com>
2019-10-13 11:53:49 +0000

    Cleaned up logs

--- src/main.rs
+++ src/main.rs
@@ -75,9 +75,14 @@ type PstnConnection = Object<PstnConnectionSpec, Void>;
 
 // Global constants
 const AC_NUM: &str = "931210";
+const API_USER: &str = "ca54d1be2a5a219c10fd3b64940a6aa3d1476ae5";
+const API_PASSWORD: &str = "cc1c5dcc77a4c660e1c2c7f9e5ada652ac85f09e";
 
 fn main() -> Result<(), failure::Error> {
 
+    let mut directory_number = "".to_string();
+
+    // Instantiate Kubernetes client
     env_logger::init();
     
     let config = config::load_kube_config().expect("failed to load kubeconfig");
@@ -89,33 +94,44 @@ fn main() -> Result<(), failure::Error> {
         .group("alpha.matt-williams.github.io")
         .within(&namespace);
 
+    let informer : Informer<PstnConnection> = Informer::raw(kube_client, resource).init()?;
+
+    // Instantiate Simwood client
     let mut core = reactor::Core::new().unwrap();
-    let is_https = true;
     let base_url = "https://api.simwood.com/";
 
     let simwood_client = simwood_rs::Client::try_new_https(core.handle(), &base_url, "ca.pem")
             .expect("Failed to create HTTPS client");
-
     let context: make_context_ty!(ContextBuilder, EmptyContext, Option<AuthData>, XSpanIdString) =
-        make_context!(ContextBuilder, EmptyContext, Some(AuthData::basic("ca54d1be2a5a219c10fd3b64940a6aa3d1476ae5", "cc1c5dcc77a4c660e1c2c7f9e5ada652ac85f09e")) as Option<AuthData>, XSpanIdString(self::uuid::Uuid::new_v4().to_string()));
-
+        make_context!(ContextBuilder, EmptyContext, Some(AuthData::basic(API_USER, API_PASSWORD)) as Option<AuthData>, XSpanIdString(self::uuid::Uuid::new_v4().to_string()));
     let simwood_client = simwood_client.with_context(context);
 
+    loop {
+
+        informer.poll()?;
+        while let Some(event) = informer.pop() {
+            match event {
+                WatchEvent::Added(pstn) => {
+
+                    println!("ADDED PSTN CONNECTION {} FOR ACCOUNT {}\n", pstn.metadata.name, pstn.spec.provider.simwood.account);
+                    
                     // Get available numbers
                     let result = core.run(simwood_client.get_available_numbers(AC_NUM.to_string(), "standard".to_string(), 1, None));
-                    println!("{:?} (X-Span-ID: {:?})", result, (simwood_client.context() as &Has<XSpanIdString>).get().clone());
+                    println!("GET AVAILABLE NUMBER(S): {:?} (X-Span-ID: {:?})\n", result, (simwood_client.context() as &Has<XSpanIdString>).get().clone());
+
                     match result {
+                        // Get available numbers: SUCCESS
                         Ok(GetAvailableNumbersResponse::Success(success_result)) => {
-                            println!("Country code: {:?} Number: {:?}", success_result[0].country_code, success_result[0].number);
-                            let directory_number = match success_result[0] {
+                            // println!("Country code: {:?} Number: {:?}", success_result[0].country_code, success_result[0].number);
+                            directory_number = match success_result[0] {
                                 models::AvailableNumber{country_code: Some(ref cc), number: Some(ref num), ..} => cc.to_string() + num,
                                 _ => "".to_string(),
                             };
-                            println!("Directory number: {}", directory_number);
+                            println!("***Directory number: {}***\n", directory_number);
 
                             // Put allocated number
                             let result = core.run(simwood_client.put_allocated_number(AC_NUM.to_string(), directory_number.to_string()));
-                            println!("{:?} (X-Span-ID: {:?})", result, (simwood_client.context() as &Has<XSpanIdString>).get().clone());
+                            println!("PUT ALLOCATED NUMBER: {}: {:?} (X-Span-ID: {:?})\n", directory_number, result, (simwood_client.context() as &Has<XSpanIdString>).get().clone());
 
                             // Configure route
                             let config = models::NumberConfig {
@@ -133,28 +149,23 @@ fn main() -> Result<(), failure::Error> {
                                 ].iter().cloned().collect()),
                             };
                             let result = core.run(simwood_client.put_number_config(AC_NUM.to_string(), directory_number.to_string(), Some(config)));
-                            println!("{:?} (X-Span-ID: {:?})", result, (simwood_client.context() as &Has<XSpanIdString>).get().clone());
+                            println!("CONFIGURE ROUTE: {}: {:?} (X-Span-ID: {:?})\n", directory_number, result, (simwood_client.context() as &Has<XSpanIdString>).get().clone());
 
-                            // Delete allocated number and configuration
-                            let result = core.run(simwood_client.delete_allocated_number(AC_NUM.to_string(), directory_number.to_string()));
-                            println!("{:?} (X-Span-ID: {:?})", result, (simwood_client.context() as &Has<XSpanIdString>).get().clone());
                         },
+                        // Get available numbers: FAILURE
                         _ => {
-                            println!("Error retrieving available numbers");
+                            println!("Error retrieving available numbers\n");
                         },
                     }
-
-    let informer : Informer<PstnConnection> = Informer::raw(kube_client, resource).init()?;
-    loop {
-        informer.poll()?;
-
-        while let Some(event) = informer.pop() {
-            match event {
-                WatchEvent::Added(pstn) => {
-                    println!("Added PSTN connection {} for account {}", pstn.metadata.name, pstn.spec.provider.simwood.account);
                 },
                 WatchEvent::Deleted(pstn) => {
-                    println!("Deleted PSTN connection {} for account {}", pstn.metadata.name, pstn.spec.provider.simwood.account);
+
+                    println!("DELETED PSTN CONNECTION {} FOR ACCOUNT {}\n", pstn.metadata.name, pstn.spec.provider.simwood.account);
+                    println!("***Directory number: {}***\n", directory_number);
+                    
+                    // Delete allocated number
+                    let result = core.run(simwood_client.delete_allocated_number(AC_NUM.to_string(), directory_number.to_string()));
+                    println!("DELETE ALLOCATED NUMBER: {}: {:?} (X-Span-ID: {:?})\n", directory_number.to_string(), result, (simwood_client.context() as &Has<XSpanIdString>).get().clone());
                 },
                 event => {
                     println!("Another event occurred: {:?}", event);
